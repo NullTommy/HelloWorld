ec00580a6667bc0928c27b4807025df2b98e3431
panghaichen <panghaichen@jd.com>
2017-09-01 15:42:42 +0800

    Spring的ref使用

--- src/main/java/TestSpring.java
+++ src/main/java/TestSpring.java
@@ -5,19 +5,20 @@ public class TestSpring {
 
     public static void main(String[] args) {
 //        通过String数组
-//        ApplicationContext context = new ClassPathXmlApplicationContext(new String[] { "aapplicationContext.xml" });
+//        ApplicationContext context = new ClassPathXmlApplicationContext(new String[] { "ApplicationContext.xml" });
 //        单纯通过文件名
-        ApplicationContext context = new ClassPathXmlApplicationContext("aapplicationContext.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
 
         Category c = (Category) context.getBean("d");
 
         System.out.println("名字："+c.getName());
+        System.out.println("Baby："+c.getBaby().getName());
+
     }
 }
 
 
 class Category {
-
     public int getId() {
         return id;
     }
@@ -31,5 +32,25 @@ class Category {
         this.name = name;
     }
     private int id;
+    private String name;
+    private Baby baby;
+
+    public Baby getBaby() {
+        return baby;
+    }
+
+    public void setBaby(Baby baby) {
+        this.baby = baby;
+    }
+}
+
+class Baby {
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
     private String name;
 }
